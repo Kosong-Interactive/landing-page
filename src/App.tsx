dfb414e0eb87a6baf91d9246e92773efae57d4ab
Faishal Pasa <faishal.pasa@bareksa.com>
2024-07-26 07:01:42 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className='bg-gray-950 flex flex-col justify-center items-center w-full min-h-screen'>
+      <div className='flex flex-col gap-6 sm:gap-10 p-8'>
+        <img src='/images/white-full.png' alt='logo' className='w-full max-w-sm mx-auto' />
+        <p className="text-md text-gray-300 text-center sm:text-2xl">
+          Kami mulai dari <span className="text-white font-semibold underline">nol</span>, dan kadang berhenti disitu.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
