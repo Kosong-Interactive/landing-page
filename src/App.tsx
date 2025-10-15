8e618df35ea65ad1365ba5883a911a996831dc54
Faishal Pasa <mfaishalpasa@gmail.com>
2025-10-15 08:05:10 +0700

    Update index.html with new title, description, and Open Graph/Twitter meta tags; add og-image.png; adjust logo image attributes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className='bg-gray-950 flex flex-col justify-center items-center w-full min-h-screen'>
       <div className='flex flex-col gap-6 sm:gap-10 p-8'>
-        <img src='/images/white-full.png' alt='logo' className='w-full max-w-sm mx-auto' />
+        <img src='/images/white-full.png' alt='logo' className='w-full max-w-sm mx-auto' width="100%" height="auto" />
         <p className="text-md text-gray-300 text-center sm:text-2xl">
           Kami mulai dari <span className="text-white font-semibold underline">nol</span>, dan kadang berhenti disitu.
         </p>
